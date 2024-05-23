0237ebc6e3d344ae99c44eae334d5ed4dd13c762
yoonjaeo <140726268+yoonjaeo@users.noreply.github.com>
2024-05-23 16:47:19 +0900

    create table schema

--- db/create_table.sql
+++ db/create_table.sql
@@ -10,3 +10,26 @@ CREATE TABLE student (
     email           VARCHAR(50)     UNIQUE NOT NULL
 )
 ;
+
+CREATE TABLE instance_info(
+    id              SERIAL          PRIMARY KEY,
+    instance_id     VARCHAR(20)     UNIQUE NOT NULL,
+    state           VARCHAR(10)
+)
+;
+
+CREATE TYPE
+    request_type AS ENUM ('start', 'end')
+;
+
+create table slack_instance_request_log(
+		id              SERIAL          PRIMARY KEY,
+		student_name    VARCHAR(5)		not NULl,
+		instance_id     VARCHAR(20)		not null,
+		request_type 	request_type,
+		request_time  	timestamp 		not null
+);
+
+alter table slack_instance_request_log add foreign key (student_name) references student(name);
+alter table instance_info  add foreign key (instance_id) references instance_info(instance_id);
+
