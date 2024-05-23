ed49017c0c93caa5044dff39bc5e6661f45220d4
yoonjaeo <140726268+yoonjaeo@users.noreply.github.com>
2024-05-23 18:36:38 +0900

    Add schema, instance_info and slack_instance_request_log table

--- db/create_table.sql
+++ db/create_table.sql
@@ -10,3 +10,26 @@ CREATE TABLE student (
     email           VARCHAR(50)     UNIQUE NOT NULL
 )
 ;
+
+CREATE TABLE instance_info(
+    id              SERIAL          PRIMARY KEY,
+    instance_id     VARCHAR(20)     UNIQUE NOT NULL,
+    state           VARCHAR(10)
+)
+;
+
+CREATE TYPE
+    request_type AS ENUM ('start', 'end')
+;
+
+CREATE TABLE slack_instance_request_log(
+    id              SERIAL          PRIMARY KEY,
+    student_name    VARCHAR(5)		NOT NULL,
+    instance_id     VARCHAR(20)		NOT NULL,
+    request_type 	request_type,
+    request_time  	TIMESTAMP 		NOT NULL
+)
+;
+
+ALTER TABLE slack_instance_request_log ADD FOREIGN KEY (student_name) REFERENCES student(name);
+ALTER TABLE instance_info  ADD FOREIGN KEY (instance_id) REFERENCES instance_info(instance_id);
