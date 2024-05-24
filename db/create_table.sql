93b8318fb81c8f8f94ab182136bbca39f9e4dd83
yoonjaeo <140726268+yoonjaeo@users.noreply.github.com>
2024-05-24 10:34:22 +0900

    Add table schema instance_info and slack_instance_request_log and create ERD

--- db/create_table.sql
+++ db/create_table.sql
@@ -10,3 +10,23 @@ CREATE TABLE student (
     email           VARCHAR(50)     UNIQUE NOT NULL
 )
 ;
+
+CREATE TABLE instance_info(
+    id              SERIAL          PRIMARY KEY,
+    instance_id     VARCHAR(20)     UNIQUE NOT NULL,
+    state           VARCHAR(10)
+)
+;
+
+CREATE TYPE
+    request_type AS ENUM ('start', 'end')
+;
+
+CREATE TABLE slack_instance_request_log(
+    id                  SERIAL          PRIMARY KEY,
+    student_id          INT             NOT NULL REFERENCES student(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    instance_info_id    INT             NOT NULL REFERENCES instance_info(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    request_type        request_type,
+    request_time        TIMESTAMP       NOT NULL
+)
+;
