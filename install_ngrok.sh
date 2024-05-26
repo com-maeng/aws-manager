1999504f1db8224ccddc46dfae3fa350d48977b5
Yi Hong-ju <to_hongju@pm.me>
2024-05-26 08:05:15 +0000

    Configure production-level back-end system
    Used Ngrok, Nginx, Gunicorn and Flask. The shell scripts are used to
    install dependencies.

---
+++ install_ngrok.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+	| sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+	&& echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+	| sudo tee /etc/apt/sources.list.d/ngrok.list \
+	&& sudo apt -y update \
+	&& sudo apt -y install ngrok
+
+# TODO
+# ngrok config add-authtoken AUTHTOKEN
