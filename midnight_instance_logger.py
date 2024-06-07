abd0c39b54231290dd38288d222c461c33982a93
yoonjaeo <140726268+yoonjaeo@users.noreply.github.com>
2024-06-07 14:25:12 +0900

    update usage time calculate logic and instance police

---
+++ midnight_instance_logger.py
@@ -0,0 +1,17 @@
+from instance_police import InstancePolice
+
+
+def insert_log_at_midnight():
+    '''자정마다 사용중인 인스턴스들을 system log에 적재 기능 구현.'''
+
+    instance_police = InstancePolice()
+
+    running_instance = instance_police.get_instance_running_list()
+
+    for instance_id in running_instance:
+        instance_police.timer.insert_system_logs(instance_id, 'start')
+
+
+if __name__ == "__main__":
+
+    insert_log_at_midnight()
