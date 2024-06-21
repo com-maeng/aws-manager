8b9a13c945c75ba47e58317366eb53d5c40c17e4
yoonjaeo <140726268+yoonjaeo@users.noreply.github.com>
2024-06-21 11:18:19 +0900

    리뷰 수정사항

--- client/aws_client.py
+++ client/aws_client.py
@@ -208,7 +208,7 @@ class CloudTrailClient:
         event_name: str,
         start_time: datetime,
         end_time: datetime
-    ) -> Optional[list[dict[str,]]]:
+    ) -> Optional[list[dict]]:
         ''' 지정된 시간 범위에 생성된 CloudTrail 로그들 중 해당 event name에 알맞는 log들을 추출합니다.
 
         Args:
