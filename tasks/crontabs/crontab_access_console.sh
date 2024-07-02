fc28bc892d1b873fceed7c80485903d9d46d4174
yoonjaeo <2000yoonjae@gmail.com>
2024-07-02 22:02:30 +0900

    zsh 파일로 변경

--- tasks/crontabs/crontab_access_console.sh
+++ tasks/crontabs/crontab_access_console.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
-
+#!/usr/bin/env zsh
 
 export PATH="$HOME/.pyenv/bin:$PATH"
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 
-source ~/.bashrc
+source ~/.zshrc
 
 pyenv activate 3.11.9/envs/deploy
 
