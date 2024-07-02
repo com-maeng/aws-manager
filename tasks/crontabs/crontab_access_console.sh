dd2f903f19262fd35a138543a13df341deb3c03c
yoonjaeo <140726268+yoonjaeo@users.noreply.github.com>
2024-07-02 21:23:15 +0900

    내용 저장 후 다시 로드

---
+++ tasks/crontabs/crontab_access_console.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+source ~/.bashrc
+
+pyenv activate 3.11.9/envs/deploy
+
+cd /home/hongju/aws-manager/tasks/cronjobs
+
+python console_access_manager.py
+ 
+echo "접근 권한 부여 및 회수 shell 정상 작동 $(date)" >> cronjob_log.txt 
---
+++ tasks/crontabs/crontab_instance_stop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export PATH="$HOME/.pyenv/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+source ~/.bashrc
+
+pyenv activate 3.11.9/envs/deploy
+
+cd /home/hongju/aws-manager/tasks/cronjobs
+ 
+python instance_stopper.py
+
+echo "인스턴스 6시에 종료  shell 정상 작동 $(date)" >> cronjob_log.txt 
